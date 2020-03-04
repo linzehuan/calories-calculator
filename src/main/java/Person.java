e8300ee72a5e2ae6757dd0d13514af3b7387c67e
linzehuan <lzh@kapark.cn>
2020-03-05 00:28:25 +0800

    refactor: add method to person

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -7,5 +7,22 @@
 public class Person {
 
     public Person(Double weight, Double feet, Double inches, Double age) {
+
+    }
+
+    public double femaleCalculate(Double weight, Double feet, Double inches, Double age) {
+        return 655
+                + (4.3 * weight)
+                + (4.7 * ((feet * 12)
+                + inches))
+                - (4.7 * age);
+    }
+
+    public double mateCalculate(Double weight, Double feet, Double inches, Double age) {
+        return 66
+                + (6.3 * weight)
+                + (12.9 * ((feet * 12)
+                + inches))
+                - (6.8 * age);
     }
 }
